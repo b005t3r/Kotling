470201dcf96f2664ed603354352791c815030de2
b005t3r <lazarecki@gmail.com>
2016-04-26 21:10:59 +0200

    Unit tests

--- core/src/test/kotlin/com/kotling/rendering/test/VertexFormatTest.kt
+++ core/src/test/kotlin/com/kotling/rendering/test/VertexFormatTest.kt
@@ -4,16 +4,47 @@ import com.badlogic.gdx.graphics.VertexAttributes
 import com.kotling.rendering.VertexAttributesCache
 import com.kotling.rendering.VertexFormat
 import junit.framework.TestCase
+import kotlin.test.assertNotEquals
 
 class VertexFormatTest : TestCase() {
+    fun testNoFormatException() {
+        val subject = ShouldBlowUpSubject()
+        try {
+            subject.attributes.size()
+            fail("UninitializedPropertyAccessException not thrown")
+        }
+        catch(e:UninitializedPropertyAccessException) {
+            assertEquals("property not annotated with @VertexFormat", e.message)
+        }
+    }
+
     fun testFormat() {
-        val test = Test()
-        var attrs = test.attributes
-        assertEquals(3, test.attributes.size())
+        val subjectA = TestSubjectA()
+        assertEquals(3, subjectA.attributes.size())
+
+        val subjectB = TestSubjectB()
+        assertEquals(subjectA.attributes, subjectB.attributes)
+
+        val subjectC = TestSubjectC()
+        assertNotEquals(subjectA.attributes, subjectC.attributes)
     }
 }
 
-class Test {
+class ShouldBlowUpSubject {
+    val attributes:VertexAttributes by VertexAttributesCache
+}
+
+class TestSubjectA {
     @VertexFormat("position:float2", "color:byte4", "uv:float2")
+    val attributes:VertexAttributes by VertexAttributesCache
+}
+
+class TestSubjectB {
+    @VertexFormat("position:float2", "color:byte4", "uv:float2")
+    val attributes:VertexAttributes by VertexAttributesCache
+}
+
+class TestSubjectC {
+    @VertexFormat("position:float2", "uv:float2", "color:byte4")
     val attributes:VertexAttributes by VertexAttributesCache
 }
