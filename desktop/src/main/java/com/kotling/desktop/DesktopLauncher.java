96478a828065da174a17eb17ceff32a8d9a94bb9
b005t3r <lazarecki@gmail.com>
2016-05-02 17:45:45 +0200

    Refactoring: Demo's name changed

--- desktop/src/main/java/com/kotling/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/kotling/desktop/DesktopLauncher.java
@@ -2,13 +2,12 @@ package com.kotling.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.kotling.KotlingDemo;
-import com.kotling.LibGDXDemo;
+import com.kotling.KotlingRendererDemo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		//new LwjglApplication(new LibGDXDemo(), config);
-		new LwjglApplication(new KotlingDemo(), config);
+		new LwjglApplication(new KotlingRendererDemo(), config);
 	}
 }
